7cca322b2a7e634977eff208691630fb2d94a619
cyliong <liongtheprogrammer@gmail.com>
2020-11-16 13:18:59 +0800

    Implement TTS for the selected vocabulary in VocabularyListView

--- lib/component/vocabulary_list_view.dart
+++ lib/component/vocabulary_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:spelling_practice/model/vocabulary.dart';
 
 class VocabularyListView extends StatelessWidget {
@@ -10,11 +11,16 @@ class VocabularyListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.separated(
         itemBuilder: (context, index) {
+          final vocabulary = vocabularyList[index].vocabulary;
+
           return ListTile(
-            title: Text('${vocabularyList[index].vocabulary}'),
+            title: Text(vocabulary),
             trailing: IconButton(
               icon: Icon(Icons.record_voice_over),
-              onPressed: () {},
+              onPressed: () async {
+                FlutterTts tts = FlutterTts();
+                await tts.speak(vocabulary);
+              },
             ),
           );
         },
