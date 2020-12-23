dd2818ff80729dbf89961c9e322fb066f96c762e
cyliong <liongtheprogrammer@gmail.com>
2020-12-23 23:03:07 +0800

    Convert SettingsPage to a StatefulWidget

--- lib/page/settings_page.dart
+++ lib/page/settings_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
