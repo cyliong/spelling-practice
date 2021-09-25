d311cd3bf7c24f28d5915050330829f3c0e2532c
cyliong <liongtheprogrammer@gmail.com>
2021-09-25 23:18:20 +0800

    Provide a const constructor for the App class

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ void main() async {
 }
 
 class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
